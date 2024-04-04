017f48c04798fb474bda7859d523bcce2f441c20
Santiago Acuña <santfloo7xcrl8@gmail.com>
2024-04-04 14:20:34 -0300

    Mongo DB connection

--- API/server.js
+++ API/server.js
@@ -5,11 +5,19 @@ import errorHandler from "./src/middlewares/errorhandler.mid.js";
 import pathHandler from "./src/middlewares/pathHandler.mid.js";
 import __dirname from "./utils.js";
 import apiRouter from "./src/routes/index.router.js";
+import db from "./src/utils/dbConnection.js";
 
 const server = express();
 const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => console.log(`server ready on port ${PORT}`));
+
+try {
+  await db();
+  console.log("DB connected");
+} catch (e) {
+  console.log(e);
+}
 
 // Global Middlewares
 server.use(
@@ -19,13 +27,10 @@ server.use(
   })
 );
 
-
-server.use("/api", apiRouter)
+server.use("/api", apiRouter);
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-route(server);
 server.use(errorHandler);
 server.use(pathHandler);
-server.use(express.static(__dirname + "/public"))
-
+server.use(express.static(__dirname + "/public"));
--- API/src/utils/dbConnection.js
+++ API/src/utils/dbConnection.js
@@ -1,6 +1,14 @@
-import { connect } from "mongoose";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const { MONGO_USER, MONGO_PASSWORD } = process.env;
 
-export const dbConnection = async () => {
-    await connect(/*aqui iria el link de mongo*/)
-    console.log("db connected")
-}
+// URL de la base de datos
+const dbURI = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.zjtkxvt.mongodb.net/nombre_de_la_base_de_datos?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Conectar a la base de datos
+const db = async () => {
+  mongoose.connect(dbURI);
+};
+
+export default db;
