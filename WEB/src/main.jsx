c6f833e735dcaebaac953894783409447e36fd4a
emaenriquez <emaenriquez1985@gmail.com>
2024-04-04 17:13:23 -0300

    installed Tailwind CSS and Material UI for the project

--- WEB/src/main.jsx
+++ WEB/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Example from './Example.jsx'
 import './index.css'
+import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+      <Example />
+    </ThemeProvider>
+
   </React.StrictMode>,
 )
