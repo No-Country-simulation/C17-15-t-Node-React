ad60e34831117ecc9088fcf0da23161f681db935
juanpintoselso33 <juanpintoselso33@gmail.com>
2024-04-05 16:00:55 -0300

    Rating y tutor actualizados

--- API/src/controllers/adminController.js
+++ API/src/controllers/adminController.js
@@ -2,72 +2,71 @@ import { admin } from "../data/mongoManager.js";
 
 class AdminController {
   constructor() {
-    this.controller = admin
+    this.controller = admin;
   }
 
-  create = async ( req, res, next ) => {
+  create = async (req, res, next) => {
     try {
-      const data = req.body
-      const response = await this.controller.create(data)
+      const data = req.body;
+      const response = await this.controller.create(data);
       return res.json({
         statusCode: 201,
-        response
-      })
+        response,
+      });
     } catch (error) {
-      return next(error)
+      return next(error);
     }
-  }
-  read = async(req, res, next) => {
+  };
+  read = async (req, res, next) => {
     try {
-      const response = await this.controller.read()
+      const response = await this.controller.read();
       return res.json({
         statusCode: 200,
-        response
-      })
+        response,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
-  readOne = async(req, res, next) => {
+  };
+  readOne = async (req, res, next) => {
     try {
-      const { aid } = req.params
-      const response = await this.controller.readOne(aid)
-      return res.json({
-        statusCode:200,
-        response
-      })
-    } catch (error) {
-      next(error)
-    }
-  }
-  destroy = async(req, res, next) => {
-    try {
-      const { aid } = req.params
-      const response = await this.controller.delete(aid)
+      const { aid } = req.params;
+      const response = await this.controller.readOne(aid);
       return res.json({
         statusCode: 200,
-        response
-      })
+        response,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
-  update = async(req, res, next) => {
+  };
+  destroy = async (req, res, next) => {
     try {
-      const { aid } = req.params
-      const data = req.body
-      const opts = { new: true }
-      const response = await this.controller.update(aid, data, opts)
+      const { aid } = req.params;
+      const response = await this.controller.delete(aid);
       return res.json({
         statusCode: 200,
-        response
-      })
+        response,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
+  };
+  update = async (req, res, next) => {
+    try {
+      const { aid } = req.params;
+      const data = req.body;
+      const opts = { new: true };
+      const response = await this.controller.update(aid, data, opts);
+      return res.json({
+        statusCode: 200,
+        response,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
 }
 
-const controller = new AdminController(admin)
-export const { create, read, readOne, destroy, update } = controller
-
+const controller = new AdminController(admin);
+export const { create, read, readOne, destroy, update } = controller;
