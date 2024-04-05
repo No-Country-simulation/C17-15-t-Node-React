142e87c8a41788d13b351d11b5f207c34f98de81
Facundo Medina <facutota@gmail.com>
2024-04-05 15:25:29 -0300

    adaptar subject

--- API/src/controllers/subjectController.js
+++ API/src/controllers/subjectController.js
@@ -1,63 +1,75 @@
-import { Subject } from "../data/model/index.js";
+import { subject } from "../data/mongoManager.js";
 
-// Obtener todas las asignaturas
-export const getAllSubjects = async (req, res) => {
-  try {
-    const subjects = await Subject.find();
-    res.status(200).json(subjects);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+class SubjectController {
+  constructor() {
+    this.controller = subject
   }
-};
-// Obtener una asignatura específica por ID
-export const getSubjectById = async (req, res, next) => {
-  try {
-    const subject = await Subject.findById(req.params.id);
-    if (!subject) {
-      return res.status(404).json({ message: "Subject not found" });
-    }
-    res.status(200).json(subject);
-  } catch (error) {
-    next(error)
-  }
-};
 
-// Crear una nueva asignatura
-export const createSubject = async (req, res) => {
-  try {
-    const newSubject = new Subject(req.body);
-    const savedSubject = await newSubject.save();
-    res.status(201).json(savedSubject);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
-// Editar una asignatura
-export const updateSubject = async (req, res) => {
-  try {
-    const updatedSubject = await Subject.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    );
-    if (!updatedSubject) {
-      return res.status(404).json({ message: "Subject not found" });
+  create = async ( req, res, next ) => {
+    try {
+      const data = req.body
+      const response = await this.controller.create(data)
+      return res.json({
+        statusCode: 201,
+        response
+      })
+    } catch (error) {
+      return next(error)
     }
-    res.status(200).json(updatedSubject);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
   }
-};
+  read = async(req, res, next) => {
+    try {
+      const response = await this.controller.read()
+      return res.json({
+        statusCode: 200,
+        response
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
+  readOne = async(req, res, next) => {
+    try {
+      const { aid } = req.params
+      const response = await this.controller.readOne(aid)
+      return res.json({
+        statusCode:200,
+        response
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
+  destroy = async(req, res, next) => {
+    try {
+      const { aid } = req.params
+      const response = await this.controller.delete(aid)
+      return res.json({
+        statusCode: 200,
+        response
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
+  update = async(req, res, next) => {
+    try {
+      const { aid } = req.params
+      const data = req.body
+      const opts = { new: true }
+      const response = await this.controller.update(aid, data, opts)
+      return res.json({
+        statusCode: 200,
+        response
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
+}
 
-// Borrar una asignatura
-export const deleteSubject = async (req, res) => {
-  try {
-    const deletedSubject = await Subject.findByIdAndDelete(req.params.id);
-    if (!deletedSubject) {
-      return res.status(404).json({ message: "Subject not found" });
-    }
-    res.status(200).json({ message: "Subject deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+const controller = new SubjectController(subject)
+export const { create, read, readOne, destroy, update } = controller
+
+
+
