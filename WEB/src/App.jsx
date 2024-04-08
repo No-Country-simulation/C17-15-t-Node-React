217bebbd37a210bc309942b1889ad54a1f74acba
Yesiblato <87024446+Yesiblato@users.noreply.github.com>
2024-04-08 20:39:16 -0300

    Se agrega el archivo Testimonial para la seccion de testimonios

--- WEB/src/App.jsx
+++ WEB/src/App.jsx
@@ -3,11 +3,13 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 import { Footer } from './components/Footer'
+import { Testimonial } from './components/Testimonial'
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+      <Testimonial />
       <Footer />
     </>
     )
