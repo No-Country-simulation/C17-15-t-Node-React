82bae8a8dc6bbd37d16d72363c4a2791d9d1084b
emaenriquez <emaenriquez1985@gmail.com>
2024-04-09 20:58:38 -0300

    section tutorias

---
+++ WEB/src/components/MenuCardSectionEdicionVideo.jsx
@@ -0,0 +1,59 @@
+
+import BookingCard from "./BookingCard"
+
+export default function MenuCardSectionEdicionVideo() {
+    return (
+        <>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4 p-2">
+                <BookingCard
+                    imagen="https://plus.unsplash.com/premium_photo-1664302522743-2fba3439ea1b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    title="WoorPress"
+                    ranking="2.0"
+                    description="Enter a freshly updated and thoughtfully furnished peaceful home surrounded by ancient trees, stone walls, and open meadows."
+                    storeName="300$"
+                ></BookingCard>
+
+                <BookingCard
+                    imagen="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                    title="WoorPress"
+                    ranking="2.0"
+                    description="Enter a freshly updated and thoughtfully furnished peaceful home surrounded by ancient trees, stone walls, and open meadows."
+                    storeName="300$"
+                ></BookingCard>
+
+                <BookingCard
+                    imagen="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                    title="WoorPress"
+                    ranking="2.0"
+                    description="Enter a freshly updated and thoughtfully furnished peaceful home surrounded by ancient trees, stone walls, and open meadows."
+                    storeName="300$"
+                ></BookingCard>
+
+                <BookingCard
+                    imagen="https://plus.unsplash.com/premium_photo-1664302522743-2fba3439ea1b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    title="WoorPress"
+                    ranking="2.0"
+                    description="Enter a freshly updated and thoughtfully furnished peaceful home surrounded by ancient trees, stone walls, and open meadows."
+                    storeName="300$"
+                ></BookingCard>
+
+                <BookingCard
+                    imagen="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                    title="WoorPress"
+                    ranking="2.0"
+                    description="Enter a freshly updated and thoughtfully furnished peaceful home surrounded by ancient trees, stone walls, and open meadows."
+                    storeName="300$"
+                ></BookingCard>
+
+                <BookingCard
+                    imagen="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                    title="WoorPress"
+                    ranking="2.0"
+                    description="Enter a freshly updated and thoughtfully furnished peaceful home surrounded by ancient trees, stone walls, and open meadows."
+                    storeName="300$"
+                ></BookingCard>
+
+            </div>
+        </>
+    )
+}
