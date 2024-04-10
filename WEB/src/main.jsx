dd5e77af70fae2f0767ecf5f30762c1d56363cc8
Yesiblato <87024446+Yesiblato@users.noreply.github.com>
2024-04-10 17:48:03 -0300

    Se arrreglan los inconvenientes con el main.jsx

--- WEB/src/main.jsx
+++ WEB/src/main.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
 import App from './App.jsx'
-=======
-import App from './App'
-// import Example from './Example.jsx'
->>>>>>> 38914d73c51d1020a1ae8db8b265b1f63c67746a
 import './index.css'
 import { ThemeProvider } from "@material-tailwind/react";
 
@@ -13,10 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
-<<<<<<< HEAD
-=======
-      {/* <Example /> */}
->>>>>>> 38914d73c51d1020a1ae8db8b265b1f63c67746a
     </ThemeProvider>
   </React.StrictMode>,
 )
