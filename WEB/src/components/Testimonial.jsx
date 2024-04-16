735a7a41883bc0b374635c6067ef13d3fff622a5
Yesiblato <87024446+Yesiblato@users.noreply.github.com>
2024-04-16 17:42:52 -0300

    Se arregla vista del componente testimonios y footer

--- WEB/src/components/Testimonial.jsx
+++ WEB/src/components/Testimonial.jsx
@@ -6,37 +6,21 @@ import {
   Avatar,
 } from "@material-tailwind/react";
 
-function StarIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="h-5 w-5 text-yellow-700"
-    >
-      <path
-        fillRule="evenodd"
-        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
-}
 
 export function Testimonial() {
   return (
     <div className="flex flex-col p-8 m-auto gap-6 items-center justify-center ">
       <div className="flex flex-col items-center justify-center">
-        <div className="lg:text-5xl font-extrabold mb-8 cellphone:text-4xl cellphone:flex">
-          <span className="bg-clip-text text-transparent bg-gradient-to-br from-primary to-secondary cellphone:text-center">Testimonios que inspiran</span>
+        <div className="text-5xl font-extrabold mb-8">
+          <span className="bg-clip-text text-transparent bg-gradient-to-br from-primary to-secondary">Testimonios que inspiran</span>
         </div>
-        <Typography className="lg:mb-5 cellphone:text-center">
+        <Typography className="mb-5">
           Descubre lo que dicen nuestros estudiantes sobre sus experiencias con
           MasterAula
         </Typography>
       </div>
 
-      <div className="lg:flex lg:m-auto gap-6 items-center justify-center cellphone:flex-col">
+      <div className="flex lg:m-auto gap-6 items-center justify-center">
         <Card
           color="transparent"
           shadow={false}
