fa7dcaaae6b42788c44d2d4266bddfa7cc2b0e9e
emaenriquez <emaenriquez1985@gmail.com>
2024-04-16 16:57:45 -0300

    enrutamiento

--- WEB/src/components/Navbar.jsx
+++ WEB/src/components/Navbar.jsx
@@ -1,42 +1,28 @@
-import "../styles/Navbar.css";
-import { BrowserRouter as Router, Routes, Route, NavLink, Outlet } from 'react-router-dom';
 
-import { About } from "./pages/About"
-import { Contact } from "./pages/Contact"
-import { Home } from "./pages/Home"
-import { Services } from "./pages/Services"
+import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
-  return (
-    // modifique casi todo para que sirviera de demostracion
-    <Router>
-      <div>
-        <nav className="navbar">
-          <ul className="navbar-ul">
-            <li className="navbar-li">
-              <NavLink className="navbar-a" exact to="/">Home</NavLink>
-            </li>
-            <li className="navbar-li">
-              <NavLink className="navbar-a" to="/about">About</NavLink>
-            </li>
-            <li className="navbar-li">
-              <NavLink className="navbar-a" to="/services">Services</NavLink>
-            </li>
-            <li className="navbar-li">
-              <NavLink className="navbar-a" to="/contact">Contact</NavLink>
-            </li>
-          </ul>
-        </nav>
 
-        <Routes>
-          <Route path='/' element={<Outlet />}>
-            <Route index element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/contact" element={<Contact />} />
-          </Route>
-        </Routes>
-      </div>
-    </Router>
+  const links = [
+    { to: '/', label: 'Home' },
+    { to: '/about', label: 'About' },
+    { to: '/contact', label: 'Contact' },
+    { to: '/services', label: 'services' },
+  ];
+
+  return (
+
+    <nav>
+      <ul>
+        {links.map((link, index) => (
+          <li key={index}>
+            <NavLink to={link.to}>
+              {link.label}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+
   );
 };
---
+++ WEB/src/pages/Contact.jsx
@@ -0,0 +1,5 @@
+export default function Contact(){
+  return (<>
+    <h1>Hola mundo desde Contact</h1>
+  </>);
+};
