c3d3fa877beed8839f8dbfe9f3ff9d86bcec827a
Yesiblato <87024446+Yesiblato@users.noreply.github.com>
2024-04-16 17:27:17 -0300

    Se arregla el problema de React Router Dom

--- WEB/src/App.jsx
+++ WEB/src/App.jsx
@@ -1,39 +1,24 @@
-import './App.css'
-// import { Footer } from './components/Footer'
-// import { Testimonial } from './components/Testimonial'
-import { Navbar } from './components/Navbar';
-// import MenuTutorias from "./components/MenuTutorias";
-// import ProfesorDestacado from './components/ProfesorDestacado';
-
-// paginas
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import { About } from "./pages/About"
-import { Services } from "./pages/Services"
-
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import  Home  from './pages/Home'; // Assuming Home is imported
+import  Contact  from './pages/Contact';
+import { About } from './pages/About';
+import { Services } from './pages/Services';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
 
 const App = () => {
   return (
-    <>
-
-      <Router>
-        <Navbar></Navbar>
-        <Routes>
-          <Route path='/' element={<Home></Home>}></Route>
-          {/* <Route path='/home' element={<Home></Home>}></Route> */}
-          <Route path='/contact' element={<Contact></Contact>}></Route>
-          <Route path='/about' element={<About></About>}></Route>
-          <Route path='/services' element={<Services></Services>}></Route>
-        </Routes>
-      </Router>
-
-      {/* <Testimonial/>
-      <ProfesorDestacado/>
-      <MenuTutorias></MenuTutorias> */}
-      
-      {/* <Footer /> */}
-    </>
+    <Router>
+      <Navbar /> {/* Render Navbar before Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Descomenta y establece la ruta a "/" */}
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+      <Footer /> {/* Render Footer after Routes */}
+    </Router>
   );
 };
 
