12c046ac1c172fdcd5c2acc262c72f74695b24fe
juanpintoselso33 <juanpintoselso33@gmail.com>
2024-04-16 12:11:59 -0300

    Agregado controladores de flujo de estudiantes a Cursos

--- API/src/controllers/courseRatingController.js
+++ API/src/controllers/courseRatingController.js
@@ -1,4 +1,5 @@
 import { courseRating } from "../data/mongoManager.js";
+import { CourseController } from "./courseController.js"; // Asegúrate de que la ruta sea correcta
 
 class CourseRatingController {
   constructor() {
@@ -10,6 +11,8 @@ class CourseRatingController {
     try {
       const data = req.body; // Datos de la calificación
       const response = await courseRating.create(data);
+      // Llamada a updateAverageRating del CourseController
+      await CourseController.updateAverageRating(data.course);
       res
         .status(201)
         .json({ message: "Rating created successfully", response });
