d8ce7ad867df4c38c326ebc1d19ca5453f2d9650
Yesiblato <87024446+Yesiblato@users.noreply.github.com>
2024-04-17 22:28:43 -0300

    Se trabaja en estilos

--- WEB/src/components/Navbar.jsx
+++ WEB/src/components/Navbar.jsx
@@ -43,8 +43,8 @@ export const NavBar = () => {
 
   return (
     <>
-      <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] p-5  ">
-        <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-gradient-to-br from-primary to-secondary mb-10">
+      <div className="">
+        <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-gradient-to-br from-primary to-secondary">
           <div className="flex items-center justify-between text-white">
             <Typography
             variant="h2"
