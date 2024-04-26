69caabd2ce0098b3ac3594e44d92464f42897246
Agustín Barrera <140979402+agustinbarrera99@users.noreply.github.com>
2024-04-25 22:27:54 -0300

    cambios de agustin (#62)

--- WEB/src/components/MenuCardSection/MenuCardSectionDestacado.jsx
+++ WEB/src/components/MenuCardSection/MenuCardSectionDestacado.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import BookingCard from "./BookingCard";
 import { motion } from "framer-motion";
 import Loader from "/src/components/Loader"
+import axios from "axios";
 export default function MenuCardSectionDesarrollo() {
   const [courses, setCourses] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -9,18 +10,15 @@ export default function MenuCardSectionDesarrollo() {
     async function fetchCourses() {
       setIsLoading(true);
       try {
-        const response = await fetch(
-          "https://c17-15-t-node-react.onrender.com/api/courses"
+        const response = await axios.get(
+          "https://c17-15-t-node-react.onrender.com/api/courses?limit=200"
         );
-        if (response.ok) {
-          const data = await response.json();
-          console.log(data);
-          const limitedCourses = data.response.docs.slice(0, 6); // Limitando a 6 elementos
-          setCourses(limitedCourses);
-          setIsLoading(false);
-        } else {
-          throw new Error("Error al cargar los cursos");
-        }
+        const limitedCourses = response.data.response.docs.slice(0, 6);
+        limitedCourses.sort((a, b) => b.avg_rating - a.avg_rating);
+
+        console.log(limitedCourses);
+        setCourses(limitedCourses);
+        setIsLoading(false);
       } catch (error) {
         console.error(error);
       }
@@ -28,6 +26,7 @@ export default function MenuCardSectionDesarrollo() {
 
     fetchCourses();
   }, []);
+
 
   return (
     <>
